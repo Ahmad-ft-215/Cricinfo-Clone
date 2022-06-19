f7fec63f92fef63a19c7930ac840e99dcd8c552d
Ahmad Noor <99959745+Ahmad-ft-215@users.noreply.github.com>
2022-06-19 17:31:37 +0530

    front-end updated

---
+++ Front-end/app/src/App.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+//import './App.css';
+//import Footer from './components/Footer'
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+// import { Series } from './components/Series/currentcricket';
+
+function App() {
+  return (
+    <div className="App"> 
+      <Header />
+      <Navbar />
+      {/* <Footer />  */}
+    </div>
+  );
+}
+
+export default App;
