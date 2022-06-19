bd859762e54f56d787810b8478e7bdbfce05d823
Ahmad Noor <99959745+Ahmad-ft-215@users.noreply.github.com>
2022-06-19 17:23:09 +0530

    updated

---
+++ Back-end/news/index.js
@@ -0,0 +1,79 @@
+const express=require("express")
+const app=express()
+var cors = require('cors')
+const data=require("./data")
+var fs = require('fs');
+const path = require('path')
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+
+app.use(cors())
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get("/teams",(req,res)=>{
+ 
+  fs.readFile('./jsonfiles/db.json', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    return res.end(data);
+  });
+
+})
+
+app.get("/homepage",(req,res)=>{
+
+  fs.readFile('./jsonfiles/homepage.json', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    return res.end(data);
+  });
+
+})
+
+app.get("/teamsnews",(req,res)=>{
+
+  fs.readFile('./jsonfiles/news.json', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    return res.end(data);
+  });
+
+})
+app.get("/ranking",(req,res)=>{
+
+  fs.readFile('./jsonfiles/ranking.json', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    return res.end(data);
+  });
+
+})
+
+app.get("/schedules",(req,res)=>{
+
+  fs.readFile('./jsonfiles/schedules.json', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    return res.end(data);
+  });
+
+})
+
+
+app.get("/news",(req,res)=>{
+    res.end(JSON.stringify(data))
+})
+
+app.get("/news/:id",(req,res)=>{
+  const {id}=req.params
+  data.map(elem=>{
+    if(id==elem.id){
+        res.end(JSON.stringify(elem))
+    }
+  })
+})
+
+
+
+const PORT = process.env.PORT || 3030
+
+app.listen(PORT,()=>{
+    console.log("server started at 3030")
+})
